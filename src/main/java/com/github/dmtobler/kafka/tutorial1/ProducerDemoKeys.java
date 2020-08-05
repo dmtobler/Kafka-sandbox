4a8438e320fd0d646dbc5015eb523794b463f02b
Danielle Tobler <danielle.tobler210@gmail.com>
2020-08-05 13:05:35 -0500

    Created producer class with keys to illustrate key mapping to partitions

--- src/main/java/com/github/dmtobler/kafka/tutorial1/ProducerDemoKeys.java
+++ src/main/java/com/github/dmtobler/kafka/tutorial1/ProducerDemoKeys.java
@@ -6,9 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class ProducerDemoKeys {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         final Logger logger = LoggerFactory.getLogger(ProducerDemoKeys.class);
 
         String bootstrapServers = "localhost:9092";
@@ -25,7 +26,27 @@ public class ProducerDemoKeys {
         for(int i = 0; i < 10; i++) {
             // Create a Producer record
 
-            final ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "hello world" + Integer.toString(i));
+            String topic = "first_topic";
+            String value = "hello world" + Integer.toString(i);
+            String key = "id_" + Integer.toString(i);
+
+            final ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, key, value);
+
+            logger.info("Key: " + key); // log the key
+            // id_0 -> Partition 1
+            // id_1 -> Partition 0
+            // id_2 -> Partition 2
+            // id_3 -> Partition 0
+            // id_4 -> Partition 2
+            // id_5 -> Partition 2
+            // id_6 -> Partition 0
+            // id_7 -> Partition 2
+            // id_8 -> Partition 1
+            // id_9 -> Partition 2
+
+            // Re-running code shows that the same key always goes to the same partition
+            // *** By providing a key, we guarantee that the same key always goes to the same partition ***
+            // NOTE: The keys always go to the same partition for a fixed number of partitions (e.g. if we had 5 partitions versus 3 that we set up here, we would get a different mapping. However if we set up another 5 partition
 
             // Send data - asynchronous
             producer.send(record, new Callback() {
@@ -44,7 +65,7 @@ public class ProducerDemoKeys {
                         logger.error("Error while producing", e);
                     }
                 }
-            });
+            }).get(); // Block the send to make it synchronous - don't do this in production!
         }
 
         // Flush data - forces all data to be produced
